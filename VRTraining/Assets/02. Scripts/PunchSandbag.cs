ad2e6984900a1eaa9291ba303788105441b2d5ac
Wonhuijae <whj46262@gmail.com>
2024-12-02 22:38:02 +0900

    펀치 구현

---
+++ VRTraining/Assets/02. Scripts/PunchSandbag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class PunchSandbag : MonoBehaviour
+{
+    // 힘 계수
+    public float power = 5f;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name.Contains("Hand"))
+        {
+            rb.AddForce(other.transform.forward * power);
+        }
+    }
+}
