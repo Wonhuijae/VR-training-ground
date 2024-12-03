1c198f15a86cb1c99531ba2acd487a7d93915705
Wonhuijae <whj46262@gmail.com>
2024-12-03 17:00:51 +0900

    탄흔 효과 추가

--- VRTraining/Assets/02. Scripts/Gun.cs
+++ VRTraining/Assets/02. Scripts/Gun.cs
@@ -7,21 +7,35 @@ public class Gun : MonoBehaviour
     [SerializeField]
     Transform muzzle;
 
+    // 피격 위치 효과
+    [SerializeField]
+    GameObject[] bulletHoles;
+    
     public float maxDistance = 10f;
     // 총알 발사 효과
-    public GameObject shotEffect;
-    // 피격 위치 효과
-    public GameObject bulletEffect;
+    public ParticleSystem shotEffect;
 
+    int bulletCount = 0;
+    private void Awake()
+    {
+        bulletCount = bulletHoles.Length;
+    }
+
+    // 총알 발사
     public void Fire()
     {
-        Instantiate(shotEffect, muzzle.position, muzzle.rotation);
+        shotEffect.Play();
 
         RaycastHit hit;
-        Physics.Raycast(transform.position, transform.forward, out hit, maxDistance);
+        Physics.Raycast(muzzle.transform.position, muzzle.transform.forward, out hit, maxDistance);
+
         if (hit.collider != null)
         {
-            Instantiate(bulletEffect, hit.transform.position, Quaternion.identity);
+            int idx = Random.Range(0, bulletCount);
+            var hole = Instantiate(bulletHoles[idx], hit.point, hit.transform.rotation);
+            hole.transform.position += new Vector3(0, 0, 0.01f);
+
+            Destroy(hole, 3f);
         }
     }
 }
