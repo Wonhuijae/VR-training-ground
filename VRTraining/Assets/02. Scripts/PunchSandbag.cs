f8b95109bf9d94a84ce79ff8df381ddc05de8d4e
Wonhuijae <whj46262@gmail.com>
2024-12-04 14:50:01 +0900

    힘에 비례하여 Punch가 강해지도록 수정

--- VRTraining/Assets/02. Scripts/PunchSandbag.cs
+++ VRTraining/Assets/02. Scripts/PunchSandbag.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 
 public class PunchSandbag : MonoBehaviour
 {
-    // 힘 계수
-    public float power = 5f;
+    [SerializeField]
+    InputActionReference leftAction;
+    [SerializeField]
+    InputActionReference rightAction;
+
+    // 기본 힘
+    public int power = 10;
+
     // 피격음
     public AudioClip[] hitClip;
     int hitClipCount = 0;
@@ -24,12 +31,26 @@ public class PunchSandbag : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name.Contains("Hand"))
-        {
-            rb.AddForce(other.transform.forward * power);
+        Vector3 leftVelocity = leftAction.action.ReadValue<Vector3>();
+        Vector3 rightVelocity = rightAction.action.ReadValue<Vector3>();
 
-            int idx = Random.Range(0, hitClipCount);
-            audioSource.PlayOneShot(hitClip[idx]);
+        if (other.gameObject.name.Contains("LeftHand"))
+        {
+            ForceToSandbag(other.transform.forward, leftVelocity);
         }
+        else if (other.gameObject.name.Contains("RightHand"))
+        {
+            ForceToSandbag(other.transform.forward, rightVelocity);
+        }
+
+        int idx = Random.Range(0, hitClipCount);
+        audioSource.PlayOneShot(hitClip[idx]);
+    }
+
+    // 타격 시점 속도에 비례하여 힘 증가
+    void ForceToSandbag(Vector3 _direction, Vector3 _velocity)
+    {
+        float v = _velocity.magnitude;
+        rb.AddForce(_direction * v * power);
     }
 }
